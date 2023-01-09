264c1955c59650d984c6f9352760df81f7d03afd
fernando.trindade <fernando.trindade@mpse.mp.br>
2023-01-09 09:05:42 -0300

    Passing data with event and property binding (combined)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,7 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  
+ @Output() recipeWasSelected = new EventEmitter<Recipe>();
  recipes: Recipe[] = [
   new Recipe('A test Recipe','This is simply a test','https://thebrilliantkitchen.com/wp-content/uploads/2022/08/General-Tsos-chicken.jpg.webp'),
   new Recipe('Fernando Recipe','Recipe created by the chef Fernando','https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg')
@@ -16,6 +16,10 @@ export class RecipeListComponent implements OnInit {
  constructor() { }
 
   ngOnInit(): void {
+  }
+  onRecipeSelected(recipeSelected: Recipe){
+    console.log('recipe seleted: ',recipeSelected);
+    this.recipeWasSelected.emit(recipeSelected);
   }
 
 }
